04a0ee5becc9a35db5bf5e4f344b78d65f709404
srikanthpragada <srikanthpragada@yahoo.com>
2022-07-26 08:30:32 +0530

    26-JUL

--- demo/oop/course.py
+++ demo/oop/course.py
@@ -1,7 +1,15 @@
 # course with title, duration and fee
 
 class Course:
+    # static or class attribute
+    taxrate = 8
+
+    @staticmethod
+    def get_taxrate():
+        return Course.taxrate
+
     def __init__(self, title, duration, fee):
+        # Object attributes
         self.title = title
         self.duration = duration
         self.fee = fee
@@ -21,8 +29,9 @@ class Course:
     def set_fee(self, fee):
         self.fee = fee
 
+    @property
     def net_fee(self):
-        return self.fee * 1.08
+        return self.fee + (self.fee * Course.taxrate / 100)
 
     def show(self):
         print(f"Title    : {self.title}")
@@ -30,7 +39,8 @@ class Course:
         print(f"Fee      : {self.fee}")
 
 
+print(Course.get_taxrate())  # call static method
 c1 = Course("React", 12, 2000)
 c1.set_fee(3000)
 c1.show()
-print(c1.net_fee())
+print(c1.net_fee)   # Property
--- demo/oop/person_special_methods.py
+++ demo/oop/person_special_methods.py
@@ -9,11 +9,23 @@ class Person:
     def __eq__(self, other):
         return self.name == other.name and self.age == other.age
 
+    def __gt__(self, other):
+        return self.age > other.age
+
+    def __int__(self):
+        return self.age
+
 
 p1 = Person("Ellison", 50)
-p2 = Person("Ellison", 50)
+p2 = Person("Ellison", 40)
+p3 = Person("Tom", 35)
+
+print(int(p1) + int(p2))   #  p1.__int__()
 
 print(p1, str(p1))  # p1.__str__()
-print(p1 == p2)     # p1.__eq__(p2)
-# print(p1 > p2)
+print(p1 == p2)  # p1.__eq__(p2)
+print(p1 > p3)  # p1.__gt__(p3)
+print(p2 < p3)  # p1.__gt__(p3)
 
+for p in sorted([p1, p2, p3]):
+    print(p)
