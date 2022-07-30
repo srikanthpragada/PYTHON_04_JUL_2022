bdbaa1ed95d63c7e926c37fa35ccec6294d3b731
srikanthpragada <srikanthpragada@yahoo.com>
2022-07-30 08:27:09 +0530

    30-JUL

--- demo/oop/account.py
+++ demo/oop/account.py
@@ -1,3 +1,11 @@
+class InsufficientFundsError(Exception):
+    def __init__(self, balance, amount):
+        self.message = f"Insufficient withdrawable balance : {balance} to withdraw amount :{amount}"
+
+    def __str__(self):
+        return self.message
+
+
 class Account:
     minbal = 10000
 
@@ -17,17 +25,21 @@ class Account:
         return self.balance
 
     def deposit(self, amount):
+        if amount <= 0:
+            raise ValueError(f"Invalid transaction amount :{amount}")
+
         self.balance += amount
 
     def withdraw(self, amount):
         if self.balance - Account.minbal >= amount:
             self.balance -= amount
         else:
-            raise ValueError("Insufficient funds!")
+            raise InsufficientFundsError(self.balance - Account.minbal, amount)
 
 
 a1 = Account(1, "Larry")
 a1.deposit(10000)
+a1.deposit(-1000)
 a1.withdraw(5000)
 print(a1.get_balance())
 
---
+++ demo/oop/generator_demo.py
@@ -0,0 +1,16 @@
+def squares():   # Generator 
+    for n in range(1, 6):
+        yield n * n
+
+
+g = squares()
+print(type(g))
+print(next(g))
+print(next(g))
+
+for s in squares():
+    print(s)
+
+sqrs = (n * n for n in range(1, 6))  # Generator Expression
+print(sqrs)
+
